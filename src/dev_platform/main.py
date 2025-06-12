2ad3bf619c3cef35bcdec1354ee294ef58c6cd6a
Sergio Pereira <sergiopereira.br@hotmail.com>
2025-06-12 00:52:01 -0300

    evita o erro que é causado por objetos assíncronos
    mantem o CLI, porque em CLI é seguro ignorar ou simplesmente não fechar o loop explicitamente,

--- src/dev_platform/main.py
+++ src/dev_platform/main.py
@@ -1,6 +1,5 @@
 # ./src/dev_platform/main.py
 import click
-
 # Importe user_cli do user_commands (renomeado para evitar conflito)
 from dev_platform.interface.cli.user_commands import cli as user_cli
 
@@ -11,10 +10,8 @@ def main_cli():
     """CLI para o DEV Platform."""
     pass
 
-
 # Adiciona os comandos de usuário como um subgrupo 'user'
 main_cli.add_command(user_cli, name="user")
-
 
 if __name__ == "__main__":
     main_cli()
